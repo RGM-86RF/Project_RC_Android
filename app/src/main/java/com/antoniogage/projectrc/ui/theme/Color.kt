cce08fb8b0430b97012b16a91d76f969b7704a22
RGM-86RF <antoniokgage@gmail.com>
2026-01-06 11:36:29 -0500

    Initial Dev Commit

--- app/src/main/java/com/antoniogage/projectrc/ui/theme/Color.kt
+++ app/src/main/java/com/antoniogage/projectrc/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val orangeish = Color(0xFFD77F68)
+val redishPurple = Color(0xFF8F025F)
