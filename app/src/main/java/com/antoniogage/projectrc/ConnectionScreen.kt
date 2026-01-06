a4ef47258c7f22447808b7424a24edb39a86cc7f
RGM-86RF <antoniokgage@gmail.com>
2026-01-06 13:42:23 -0500

    Updated Connection Screen layout

--- app/src/main/java/com/antoniogage/projectrc/ConnectionScreen.kt
+++ app/src/main/java/com/antoniogage/projectrc/ConnectionScreen.kt
@@ -1,2 +1,35 @@
 package com.antoniogage.projectrc
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(showBackground = true)
+@Composable
+fun ConnectionScreen(){
+    Box(Modifier.fillMaxSize().padding(24.dp)){
+        IconButton({},modifier = Modifier.align(Alignment.TopStart)) {
+            Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
+        }
+        Button(
+            {},
+            Modifier.align(Alignment.BottomCenter).fillMaxWidth(0.8f)
+        )
+        {
+            Text("Connect")
+        }
+    }
+
+}
